2f4280ddac9bb743e6bd814bf690d8e073a32ddd
fafunblocked <mirzajunaid3111@gmail.com>
2025-09-26 18:23:39 +0500

    update 2

---
+++ source/class-465/js/integrator (1).js
@@ -0,0 +1,3 @@
+processGoogleToken({"newToken":"","validLifetimeSecs":300,"freshLifetimeSecs":300,"1p_jar":"","pucrd":""});
+
+
---
+++ source/class-790/js/application.js
@@ -0,0 +1,6 @@
+// Wait till the browser is ready to render the game (avoids glitches)
+window.requestAnimationFrame(function () {
+  new GameManager(7, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+});
+
+
